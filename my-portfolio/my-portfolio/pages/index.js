c248a544bd5a67ee6d8e3313ed35565dd9c12fc0
Ramez Al-Siddiq <ramez.sydney@gmail.com>
2023-06-04 15:10:32 +1000

    centered the items in the main section

--- my-portfolio/my-portfolio/pages/index.js
+++ my-portfolio/my-portfolio/pages/index.js
@@ -10,11 +10,11 @@ export default function Home() {
         <link rel='icon' href="/favicon.ico" />
       </head>
 
-      <main>
+      <main className='bg-white px-10'>
         <section className='min-h-screen'>
           <nav className='py-10 mb-12 flex justify-between'>
             <h1>Ramez Al-Siddiq</h1>
-            <ul>
+            <ul className='flex items-center'>
               <li>
                 <BsFillMoonStarsFill />
               </li>
