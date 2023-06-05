17231101d708ab76acc7b084f02cdc95e2ac4ab6
Ramez Al-Siddiq <ramez.sydney@gmail.com>
2023-06-05 10:22:29 +1000

    styled the h3 tag

--- my-portfolio/my-portfolio/pages/index.js
+++ my-portfolio/my-portfolio/pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
           </nav>
           <div className='text-center p-10'>
             <h2 className='text-5xl py-2 text-teal-600 font-medium'>Ramez Al-Siddiq</h2>
-            <h3>Junior Software Engineer.</h3>
+            <h3 className='text-2xl py-2'>Junior Software Engineer.</h3>
             <p>
               I deliver seamless digital experiences for end-users
             </p>
