bdd90cbe65ed628349822e2e22f359a091fdb03f
Ramez Al-Siddiq <ramez.sydney@gmail.com>
2023-06-05 10:09:25 +1000

    changed the font for the nav

--- my-portfolio/my-portfolio/pages/index.js
+++ my-portfolio/my-portfolio/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main className='bg-white px-10'>
         <section className='min-h-screen'>
           <nav className='py-10 mb-12 flex justify-between'>
-            <h1 className='text-xl'>Ramez Al-Siddiq</h1>
+            <h1 className='text-xl font-sans'>@ram-ram14</h1>
             <ul className='flex items-center'>
               <li>
                 <BsFillMoonStarsFill className='cursor-pointer text-2xl'/>
@@ -26,6 +26,9 @@ export default function Home() {
               </li>
             </ul>
           </nav>
+          <div>
+            <h2></h2>
+          </div>
         </section>
       </main>
     </div>
