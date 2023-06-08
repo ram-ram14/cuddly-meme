2fc37e6ef8ae1a7701a31e4c58ac6df627d35841
Ramez Al-Siddiq <ramez.sydney@gmail.com>
2023-06-08 10:19:19 +1000

    added styling to the image which resizes based on the size of the window

--- my-portfolio/my-portfolio/pages/index.js
+++ my-portfolio/my-portfolio/pages/index.js
@@ -53,7 +53,7 @@ export default function Home() {
               <AiFillLinkedin />
               <AiFillGithub />
             </div>
-            <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden'>
+            <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96'>
               <Image src={miles} layout="fill" objectFit='cover' />
             </div>
           </div>
