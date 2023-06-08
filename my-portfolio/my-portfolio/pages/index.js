f266029a3535a07c0c5b0b043a1695e11dac74e6
Ramez Al-Siddiq <ramez.sydney@gmail.com>
2023-06-08 10:17:32 +1000

    added more styling

--- my-portfolio/my-portfolio/pages/index.js
+++ my-portfolio/my-portfolio/pages/index.js
@@ -45,7 +45,7 @@ export default function Home() {
           <div className='text-center p-10'>
             <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>Ramez Al-Siddiq</h2>
             <h3 className='text-2xl py-2 md:text-3xl'>Junior Software Engineer</h3>
-            <p className='text-md py-5 leading-8 text-gray-800 md:text-xl'>
+            <p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto'>
               I deliver seamless digital experiences for end-users.
             </p>
             <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
