00e21841a3813b086c36fa959457b8101cb92ebe
Ramez Al-Siddiq <ramez.sydney@gmail.com>
2023-06-08 10:02:19 +1000

    imported all of the placeholder images for my portfolio

--- my-portfolio/my-portfolio/pages/index.js
+++ my-portfolio/my-portfolio/pages/index.js
@@ -10,6 +10,12 @@ import miles from '../public/miles-morales.jpg';
 import design from '../public/design.png';
 import code from '../public/code.png';
 import consulting from '../public/consulting.png';
+import web1 from "../public/web1.png";
+import web2 from "../public/web2.png";
+import web3 from "../public/web3.png";
+import web4 from "../public/web4.png";
+import web5 from "../public/web5.png";
+import web6 from "../public/web6.png";
 
 export default function Home() {
   return (
