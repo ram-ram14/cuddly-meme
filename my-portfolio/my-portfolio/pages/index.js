78a7bbde41b2dc81be181f35d4aa9afbc4d29e05
Ramez Al-Siddiq <ramez.sydney@gmail.com>
2023-06-08 10:39:30 +1000

    styled each of the placeholder images

--- my-portfolio/my-portfolio/pages/index.js
+++ my-portfolio/my-portfolio/pages/index.js
@@ -123,23 +123,23 @@ export default function Home() {
             </p>
           </div>
           <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
-            <div>
-              <Image src={web1} />
+            <div className='basis-1/3 flex-1'>
+              <Image src={web1} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
             </div>
-            <div>
-              <Image src={web2} />
+            <div className='basis-1/3 flex-1'>
+              <Image src={web2} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
             </div>
-            <div>
-              <Image src={web3} />
+            <div className='basis-1/3 flex-1'>
+              <Image src={web3} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
             </div>
-            <div>
-              <Image src={web4} />
+            <div className='basis-1/3 flex-1'>
+              <Image src={web4} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
             </div>
-            <div>
-              <Image src={web5} />
+            <div className='basis-1/3 flex-1'>
+              <Image src={web5} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
             </div>
-            <div>
-              <Image src={web6} />
+            <div className='basis-1/3 flex-1'>
+              <Image src={web6} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
             </div>
           </div>
         </section>
